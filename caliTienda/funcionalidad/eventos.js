fc84b452f19f930cfd35b76b17d68598a76116f9
rnicolasvergara <105612087+rnicolasvergara@users.noreply.github.com>
2022-07-31 22:29:02 -0300

    agregar script botones de inciar y crear sesion
    Se agrega un script para funcionalidad cuando tocas el boton de iniciar sesion y el de crear sesion

---
+++ caliTienda/funcionalidad/eventos.js
@@ -0,0 +1,45 @@
+let usuarios = []
+
+const iniciar = document.getElementById("IniciarSesion")
+iniciar.addEventListener ("click", respuestaClick)
+function respuestaClick(){
+    prompt("Ingresa tu usuario");
+    const pregunta = prompt("Usuario Inexistente, ¿Deseas crear uno?");
+    if (pregunta == "si"){
+        let usuario = prompt("Ingresa un nombre de usuario")
+        let nombre = prompt("Ingresa tu nombre")
+        let apellido = prompt("Ingresa tu apellido")
+        let mail = prompt ("Ingresa tu mail")
+        alert("¡Creaste tu usuario exitosamente!")
+    } else if (pregunta == "no"){
+       alert("Hasta luego, te esperamos pronto")
+        }
+
+        usuarios.push({usuario, nombre, apellido, mail})
+        console.log(usuarios)
+}
+
+const crear = document.getElementById("CrearCuenta")
+crear.addEventListener ("click", respuestaClick2)
+function respuestaClick2(){
+const pregunta2 = prompt("¿Deseas crear un usuario?");
+    if (pregunta2 == "si"){
+        let usuario2 = prompt("Ingresa un nombre de usuario")
+        let nombre2 = prompt("Ingresa tu nombre")
+        let apellido2 = prompt("Ingresa tu apellido")
+        let mail2 = prompt ("Ingresa tu mail")
+        alert("¡Creaste tu usuario exitosamente!")
+    } else if (pregunta2 == "no"){
+       alert("Hasta luego, te esperamos pronto")
+        }
+
+        usuarios.push({usuario, nombre, apellido, mail})
+        console.log(usuarios)
+}
+
+
+
+
+
+
+
--- caliTienda/funcionalidad/scripts.js
+++ caliTienda/funcionalidad/scripts.js
@@ -1,66 +1,66 @@
-const productos = [
-    { nombre: "Home Fragance AGAPE", precio: 1350},
-    { nombre: "El Perfume", precio: 1250 },
-    { nombre: "Home Spray", precio: 1250 },
-    { nombre: "Difusor", precio: 1350 },
-    { nombre: "Bubble Candle", precio: 890 },
-    { nombre: "Porta Fosforos", precio: 350 },
-];
+// const productos = [
+//     { nombre: "Home Fragance AGAPE", precio: 1350},
+//     { nombre: "El Perfume", precio: 1250 },
+//     { nombre: "Home Spray", precio: 1250 },
+//     { nombre: "Difusor", precio: 1350 },
+//     { nombre: "Bubble Candle", precio: 890 },
+//     { nombre: "Porta Fosforos", precio: 350 },
+// ];
 
-let carrito = [] 
+// let carrito = [] 
 
-let saludo = prompt("Hola ¿Queres comprar algun producto?")
+// let saludo = prompt("Hola ¿Queres comprar algun producto?")
 
-while(saludo != "si" && saludo != "no"){
-     alert("No entendi tu respuesta")
-     saludo = prompt("¿Queres comprar algun producto?")
-}
+// while(saludo != "si" && saludo != "no"){
+//      alert("No entendi tu respuesta")
+//      saludo = prompt("¿Queres comprar algun producto?")
+// }
 
-if(saludo == "si"){
-    alert("Estos son los productos disponibles")
-    let productosTotal = productos.map((producto) => producto.nombre + " $" + producto.precio);
-    alert(productosTotal.join(" - "))
-} else if (saludo == "no"){
-    alert("Hasta luego, te esperamos pronto")
-}
+// if(saludo == "si"){
+//     alert("Estos son los productos disponibles")
+//     let productosTotal = productos.map((producto) => producto.nombre + " $" + producto.precio);
+//     alert(productosTotal.join(" - "))
+// } else if (saludo == "no"){
+//     alert("Hasta luego, te esperamos pronto")
+// }
 
-while (saludo != "no"){
-    let producto = prompt ("Elegi alguno de estos productos")
-    let precio = 0
-if (producto == "Home Fragance AGAPE" || producto == "El Perfume" || producto == "Home Spray" || producto == "Difusor" || producto == "Bubble Candle" || producto == "Porta Fosforos"){
-switch(producto) {
-    case "Home Fragance AGAPE":
-        precio = 1350
-    break
-    case "El Perfume" :
-        precio = 1250
-    break
-    case "Home Spray":
-        precio = 1250
-    break
-    case "Difusor" :
-        precio = 1350
-    break
-    case "Bubble Candle":
-        precio = 890
-    break
-    case "Porta Fosforos":
-        precio = 350
-    break
-        }
+// while (saludo != "no"){
+//     let producto = prompt ("Elegi alguno de estos productos")
+//     let precio = 0
+// if (producto == "Home Fragance AGAPE" || producto == "El Perfume" || producto == "Home Spray" || producto == "Difusor" || producto == "Bubble Candle" || producto == "Porta Fosforos"){
+// switch(producto) {
+//     case "Home Fragance AGAPE":
+//         precio = 1350
+//     break
+//     case "El Perfume" :
+//         precio = 1250
+//     break
+//     case "Home Spray":
+//         precio = 1250
+//     break
+//     case "Difusor" :
+//         precio = 1350
+//     break
+//     case "Bubble Candle":
+//         precio = 890
+//     break
+//     case "Porta Fosforos":
+//         precio = 350
+//     break
+//         }
 
-    let cantidad = parseInt(prompt("¿Cuantas unidades queres?"))
+//     let cantidad = parseInt(prompt("¿Cuantas unidades queres?"))
 
-    carrito.push({producto, unidades, precio})
-    console.log(carrito)
-    } else{
-        alert("no existe ese producto")
-    }
-    saludo = prompt("¿Queres seguir comprando?")
+//     carrito.push({producto, unidades, precio})
+//     console.log(carrito)
+//     } else{
+//         alert("no existe ese producto")
+//     }
+//     saludo = prompt("¿Queres seguir comprando?")
 
-    while(saludo == "no"){
-        alert("Gracias por tu compra")
-    }
-}
+//     while(saludo == "no"){
+//         alert("Gracias por tu compra")
+//     }
+// }
 
 

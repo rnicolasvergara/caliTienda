c1993791cdd4e5bc26c1162949a8375295502d09
nicolas vergara <rnicolasvergara@gmail.com>
2022-08-07 13:56:20 -0300

    Cambios en los .js
    renombrar los js y cambios en la funcionalidad

--- caliTienda/funcionalidad/iniciar_sesion.js
+++ caliTienda/funcionalidad/iniciar_sesion.js
@@ -38,6 +38,17 @@ const pregunta2 = prompt("¿Deseas crear un usuario?");
 }
 
 
+const productos = [{ id: 1, producto: "Arroz", precio: 125 },
+ { id: 2, producto: "Fideo", precio: 70 },
+ { id: 3, producto: "Pan" , precio: 50},
+ { id: 4, producto: "Flan" , precio: 100}];
+const guardarLocal = (clave, valor) => { localStorage.setItem(clave, valor) };
+
+for (const producto of productos) {
+    guardarLocal(producto.id, JSON.stringify(producto));
+}
+
+
 
 
 
--- caliTienda/funcionalidad/scripts.js
+++ caliTienda/funcionalidad/scripts.js
@@ -1,66 +1,51 @@
-// const productos = [
-//     { nombre: "Home Fragance AGAPE", precio: 1350},
-//     { nombre: "El Perfume", precio: 1250 },
-//     { nombre: "Home Spray", precio: 1250 },
-//     { nombre: "Difusor", precio: 1350 },
-//     { nombre: "Bubble Candle", precio: 890 },
-//     { nombre: "Porta Fosforos", precio: 350 },
-// ];
+const productos = [
+    { nombre: "Home Fragance AGAPE", precio: 1350},
+    { nombre: "El Perfume", precio: 1250 },
+    { nombre: "Home Spray", precio: 1250 },
+    { nombre: "Difusor", precio: 1350 },
+    { nombre: "Bubble Candle", precio: 890 },
+    { nombre: "Porta Fosforos", precio: 350 },
+];
 
-// let carrito = [] 
+let carrito = [] 
 
-// let saludo = prompt("Hola ¿Queres comprar algun producto?")
+    let productosTotal = productos.map((producto) => producto.nombre + " $" + producto.precio);
+    alert(productosTotal.join(" - "))
 
-// while(saludo != "si" && saludo != "no"){
-//      alert("No entendi tu respuesta")
-//      saludo = prompt("¿Queres comprar algun producto?")
-// }
+let precio = 0
+if (producto == "Home Fragance AGAPE" || producto == "El Perfume" || producto == "Home Spray" || producto == "Difusor" || producto == "Bubble Candle" || producto == "Porta Fosforos"){
+switch(producto) {
+    case "Home Fragance AGAPE":
+        precio = 1350
+    break
+    case "El Perfume" :
+        precio = 1250
+    break
+    case "Home Spray":
+        precio = 1250
+    break
+    case "Difusor" :
+        precio = 1350
+    break
+    case "Bubble Candle":
+        precio = 890
+    break
+    case "Porta Fosforos":
+        precio = 350
+    break
+        }
 
-// if(saludo == "si"){
-//     alert("Estos son los productos disponibles")
-//     let productosTotal = productos.map((producto) => producto.nombre + " $" + producto.precio);
-//     alert(productosTotal.join(" - "))
-// } else if (saludo == "no"){
-//     alert("Hasta luego, te esperamos pronto")
-// }
+    carrito.push({producto, unidades, precio})
+    console.log(carrito)
+}
 
-// while (saludo != "no"){
-//     let producto = prompt ("Elegi alguno de estos productos")
-//     let precio = 0
-// if (producto == "Home Fragance AGAPE" || producto == "El Perfume" || producto == "Home Spray" || producto == "Difusor" || producto == "Bubble Candle" || producto == "Porta Fosforos"){
-// switch(producto) {
-//     case "Home Fragance AGAPE":
-//         precio = 1350
-//     break
-//     case "El Perfume" :
-//         precio = 1250
-//     break
-//     case "Home Spray":
-//         precio = 1250
-//     break
-//     case "Difusor" :
-//         precio = 1350
-//     break
-//     case "Bubble Candle":
-//         precio = 890
-//     break
-//     case "Porta Fosforos":
-//         precio = 350
-//     break
-//         }
+const productos = [
+{ id: 1, producto: "Arroz", precio: 125 },
+{ id: 2, producto: "Fideo", precio: 70 },
+{ id: 3, producto: "Pan" , precio: 50},
+{ id: 4, producto: "Flan" , precio: 100}];
+const guardarLocal = (clave, valor) => { localStorage.setItem(clave, valor) };
 
-//     let cantidad = parseInt(prompt("¿Cuantas unidades queres?"))
-
-//     carrito.push({producto, unidades, precio})
-//     console.log(carrito)
-//     } else{
-//         alert("no existe ese producto")
-//     }
-//     saludo = prompt("¿Queres seguir comprando?")
-
-//     while(saludo == "no"){
-//         alert("Gracias por tu compra")
-//     }
-// }
-
-
+for (const producto of productos) {
+    guardarLocal(producto.id, JSON.stringify(producto));
+}
